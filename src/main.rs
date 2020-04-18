d682a0d7bb692fc9482bdf69023f66595c5fa1b2
laajila <issam@DESKTOP-HESRAVA.localdomain>
2020-04-18 17:40:52 +0200

    project init

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("searching for  {}", query);
+    println!("in file {}", filename);
+    println!("all args {:?}", args);
+}
