8bc3c1b0ea525b4da4b74cd1b03c253b2ed55819
laajila <issam.benabachir@gmail.com>
2020-04-18 18:01:00 +0200

    powem.txt

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
 
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let (query,filename)=parse_config(&args);
+
+    println!("in file {}", filename);
+
+    let contents= fs::read_to_string(filename)
+        .expect("something went wrong the file can't be read!");
+    
+    println!("with text: \n{}",contents);
+    
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
     let query = &args[1];
     let filename = &args[2];
 
-    println!("searching for  {}", query);
-    println!("in file {}", filename);
-    println!("all args {:?}", args);
+    (query, filename)
 }
